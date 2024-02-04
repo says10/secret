32527bf4238ae4a90d551d831ae4a4167686ca71
says10 <142166942+says10@users.noreply.github.com>
2024-02-05 02:21:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
     clientID:process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://secret-vtyc.onrender.com/auth/google/secrets",
     //userProfileURL:"https://googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
